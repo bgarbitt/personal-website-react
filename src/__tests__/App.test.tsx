90a6e90499724f70e7890d54c9a4405eaebb1dc1
Brett Garbitt <brett.garbitt@gmail.com>
2024-09-17 18:31:14 -0600

    installing vitest
    tsconfig change enables method usages without importing

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,5 @@
+describe('<App />', () => {
+  it('basic test', () => {
+    expect(true).toBe(true)
+  })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
@@ -5,4 +6,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  test: {
+    globals: true,
+  },
 })
