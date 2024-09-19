3c6640af0283fae6c51547123956ad65ebfd1eea
Brett Garbitt <brett.garbitt@gmail.com>
2024-09-18 20:07:05 -0600

    auto sorting tailwind classnames

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,13 @@
 import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat()
 
 export default antfu({
   react: true,
-}, {
+}, ...compat.config({
+  extends: ['plugin:tailwindcss/recommended'],
   rules: {
     'antfu/top-level-function': ['off'],
   },
-})
+}))
